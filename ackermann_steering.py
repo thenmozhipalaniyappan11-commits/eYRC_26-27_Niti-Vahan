8794253a39b5c27d9334b12015e46bd4aa2907f5
thenmozhipalaniyappan11-commits <thenmozhipalaniyappan11@gmail.com>
2026-09-21 23:36:15 +0530

    Add files via upload

---
+++ ackermann_steering.py
@@ -0,0 +1,114 @@
+'''
+*****************************************************************************************
+*
+*  ===============================================
+*     Niti Vahan (NV) Theme of eYRC 2026-27
+*  ===============================================
+*
+*  This script is intended for implementation of Task 1A of Niti Vahan (NV) Theme.
+*
+*  Filename:         ackermann_steering.py
+*  Created:          2026
+*  Last Modified:
+*  Author:           e-Yantra Team
+*
+*  You are ONLY allowed to write your code inside the block marked
+*  "ADD YOUR IMPLEMENTATION HERE". Do not change anything outside it - the
+*  evaluation script relies on the rest of this file staying as it is.
+*
+*****************************************************************************************
+'''
+
+# Team ID:          < Team-ID >
+# Author List:      < Names of the team members who worked on this file, comma separated >
+# Filename:         ackermann_steering.py
+# Functions:        ackermann_wheel_angles
+# Global variables: < List any global variables you add, "None" if you add none >
+
+
+####################### IMPORT MODULES #######################
+import math
+import numpy as np
+##############################################################
+
+
+#################### VEHICLE CONSTANTS #######################
+WHEELBASE = 0.120           # L: distance between front and rear axle centrelines
+TRACK_WIDTH = 0.110         # W: distance between left and right wheel centre
+WHEEL_OFFSET = 0.0275       # O: distance between kingpin axis and wheel centre.
+##############################################################
+
+
+##############################################################
+############### ADD YOUR IMPLEMENTATION HERE #################
+##############################################################
+
+
+def ackermann_wheel_angles(delta):
+    '''
+    Purpose:
+    ---
+    Convert a single virtual steering angle into the two real front-wheel
+    angles, per the Ackermann geometry.
+
+    Input Arguments:
+    ---
+    `delta` :   [ float ]
+        Steering angle of the virtual centred front wheel, in radians.
+
+    Returns:
+    ---
+    `left_angle`  : [ float ]
+    `right_angle` : [ float ]
+        The two real front-wheel steering angles, in radians, using the
+        same sign convention as delta.
+
+    REMEMBER:
+    ---
+    WHEEL_OFFSET changes the effective half-track width inside each wheel's triangle.
+    '''
+
+    # Handle straight motion
+    if abs(delta) < 1e-12:
+        return 0.0, 0.0
+
+    # Effective half-track considering the wheel offset
+    half_track = (TRACK_WIDTH / 2.0) - WHEEL_OFFSET
+
+    # Radius of the virtual centred wheel
+    radius = WHEELBASE / math.tan(abs(delta))
+
+    # Inner and outer turning radii
+    inner_radius = radius - half_track
+    outer_radius = radius + half_track
+
+    # Calculate the corresponding wheel angles
+    inner_angle = math.atan2(WHEELBASE, inner_radius)
+    outer_angle = math.atan2(WHEELBASE, outer_radius)
+
+    # Positive delta = left turn
+    if delta > 0:
+        left_angle = inner_angle
+        right_angle = outer_angle
+
+    # Negative delta = right turn
+    else:
+        left_angle = -outer_angle
+        right_angle = -inner_angle
+
+    return left_angle, right_angle
+
+##############################################################
+################ END OF YOUR IMPLEMENTATION ##################
+##############################################################
+
+
+#################### DO NOT EDIT BELOW THIS LINE ####################
+
+if __name__ == "__main__":
+
+    test_angles = np.arange(-0.35, 0.35, 0.05)
+
+    for d in test_angles:
+        left, right = ackermann_wheel_angles(d)
+        print(f"delta={d}  ->  left={left}, right={right}")
